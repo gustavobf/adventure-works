44bca31e1db9aa3d42bdfc7f39ea3a09a1228209
Gustavo Batista <gustavobatistafig@hotmail.com>
2023-10-28 19:39:39 -0300

    environment

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,1 +1,3 @@
-export const CONSTANT_WORKS =  'adventure-works1' ?? 'adventure-works2'
+import { environment } from './environments/environment.prod';
+
+export const CONSTANT_WORKS = environment.ADVENTURE_PROD ?? 'adventure-works2'
